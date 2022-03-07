7a1f5afbe613a2d24149defa342c0d3b0abb77d6
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2022-03-07 12:02:26 +0100

    applied shellcheck rules

--- bash/update-force.sh
+++ bash/update-force.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-PROG=`basename $0`;
 BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # make sure script exits if any process exits unsuccessfully
 set -e
 
-# parse config file
-IMAGE=`$BIN/read-config.sh "FORCE_IMAGE"`
 
+# parse config file
+IMAGE=$("$BIN"/read-config.sh "FORCE_IMAGE")
+
+echo "$IMAGE"
 # pull latest image
 docker pull "$IMAGE"
 
