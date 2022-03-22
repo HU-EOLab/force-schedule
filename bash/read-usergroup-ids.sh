718706683715b74222172344cc7a9dee0af75cbf
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2022-03-22 08:35:47 +0100

    config.txt renamed USERNAME to USER_GROUP fixed read-usergroup-ids.sh by removing a recursion fixed LF in update-wvdb.sh

--- bash/read-usergroup-ids.sh
+++ bash/read-usergroup-ids.sh
@@ -4,12 +4,12 @@
 # fr example: "userX:groupY" will be returned as 1234:4321, with 1234 being the user ID etc.
 # PROG=`basename $0`;
 BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-USER_GROUP=$("$BIN"/read-usergroup-ids.sh)
 
-# IFS=', ' read -r -a array <<< "$string"
+USER_GROUP=$("$BIN"/read-config.sh "USER_GROUP")
+
 UNAME=$(grep -Po '^.*(?=:)' <<< $USER_GROUP)
 GNAME=$(grep -Po '[^:]+$' <<< $USER_GROUP)
 
 echo "$(id -u "$UNAME"):$(getent group "$GNAME" | awk -F: '{print $3}')"
 exit 0
-# getent group datacube | awk -F: '{print $3}'
+
