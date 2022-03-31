e0dd122cfa84f7baabc77b31f3a9bd076e9038b2
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2022-03-31 14:52:13 +0200

    fixed bad minute error

--- cron/cron.sh
+++ cron/cron.sh
@@ -6,12 +6,9 @@
 crontab -e
 
 # Replace all occurences of "force-schedule" below with the full file path of this repository!
-FORCE_SCHEDULE=/data/Dagobah/dc/force-schedule
-LOG_DIR=$"FORCE_SCHEDULE"/log
-
 # Daily updates each evening
-0 20 * * * $"FORCE_SCHEDULE"/bash/cronjobs-daily.sh  $"LOG_DIR"/cronjobs-daily-"$(date +"%Y%m%d%H%M%S")".log 2>&1
+0 20 * * * /data/Dagobah/dc/force-schedule/bash/cronjobs-daily.sh  /data/Dagobah/dc/force-schedule/log/cronjobs-daily-"$(date +"%Y%m%d%H%M%S")".log 2>&1
 
 # Weekly updates (processing)
 # 01:00 AM each monday morning
-0 1 * * 1 $"FORCE_SCHEDULE"/bash/cronjobs-weekly.sh $"LOG_DIR"/cronjobs-weekly-"$(date +"%Y%m%d%H%M%S")".log 2>&1
+0 1 * * 1 /data/Dagobah/dc/force-schedule/bash/cronjobs-weekly.sh /data/Dagobah/dc/force-schedule/log/cronjobs-weekly-"$(date +"%Y%m%d%H%M%S")".log 2>&1
