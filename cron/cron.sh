d712585e9d11ad0c5cd58bf2ec17221ce5986a93
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2022-04-08 09:58:14 +0200

    fixed cron.sh

--- cron/cron.sh
+++ cron/cron.sh
@@ -5,10 +5,10 @@
 # Schedule processes with Cron
 crontab -e
 
-# Replace all occurences of "force-schedule" below with the full file path of this repository!
+# Replace all occurrences of "force-schedule" below with the full file path of this repository!
 # Daily updates each evening
-0 20 * * * /data/Dagobah/dc/force-schedule/bash/cronjobs-daily.sh > /data/Dagobah/dc/force-schedule/log/cronjobs-daily-"$(date +"%Y%m%d%H%M%S")".log 2>&1
+0 20 * * * /data/Dagobah/dc/force-schedule/bash/cronjobs-daily.sh >> /data/Dagobah/dc/force-schedule/log/cronjobs-daily-$(date +"\%Y\%m\%d\%H\%M\%S").log 2>&1
 
 # Weekly updates (processing)
 # 01:00 AM each monday morning
-0 1 * * 1 /data/Dagobah/dc/force-schedule/bash/cronjobs-weekly.sh > /data/Dagobah/dc/force-schedule/log/cronjobs-weekly-"$(date +"%Y%m%d%H%M%S")".log 2>&1
+0 1 * * 1 /data/Dagobah/dc/force-schedule/bash/cronjobs-weekly.sh >> /data/Dagobah/dc/force-schedule/log/cronjobs-weekly-$(date +"\%Y\%m\%d\%H\%M\%S").log 2>&1
