5852a8fd2978d4b8a1cfa3295e476aa73eef4d16
Stefan Ernst <15325433+ernstste@users.noreply.github.com>
2022-09-09 15:46:11 +0200

    add email cronjob, cron log filenames reduced to date (HHMMSS is always 010001)

--- cron/cron.sh
+++ cron/cron.sh
@@ -11,4 +11,5 @@ crontab -e
 
 # Weekly updates (processing)
 # 01:00 AM each monday morning
-0 1 * * 1 /data/Dagobah/dc/force-schedule/bash/cronjobs-weekly.sh >> /data/Dagobah/dc/force-schedule/log/cronjobs-weekly-$(date +"\%Y\%m\%d\%H\%M\%S").log 2>&1
+0 1 * * 1 /data/Dagobah/dc/force-schedule/bash/cronjobs-weekly.sh >> /data/Dagobah/dc/force-schedule/log/cronjobs-weekly-$(date -d "%Y-%m-%d").log 2>&1
+0 10 * * 1 /data/Dagobah/dc/force-schedule/bash/send-email-report.sh >> /data/Dagobah/dc/force-schedule/log/cronjobs-weekly-$(date -d "%Y-%m-%d").log 2>&1
